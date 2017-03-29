6c2f91ec9f6529bbc4167db3cd62d8409a5b9898
M Gamboa <gamboa_girl@hotmail.com>
2017-03-29 12:14:40 -0700

    Add comments and remove hard coded number
    Add comments and replaced hard coded number in the while statement with
    the “breaks” variable

--- 1_break_time.py
+++ 1_break_time.py
@@ -1,12 +1,16 @@
 import time
 import webbrowser
 
-breaks = 3;
-break_cnt = 0;
+breaks = 3; # total number of breaks that this will run
+break_cnt = 0; # current break count
 
+# note the time this starts
 print("This program started on " + time.ctime())
-while (break_cnt < 3):
-    # sleep for 2 hours between loops
+
+# run this the number of total breaks
+    # sleep for 2 hours (7200 seconds) between loops
     time.sleep(7200)
+    # open the web browser to a Rick roll
     webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    # increment the break count by 1
     break_cnt = break_cnt + 1
